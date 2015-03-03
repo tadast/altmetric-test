eb6e54364db81999dd5e189c5ddd87a88b4dfc44
Tadas Tamosauskas <tadas@pdfcv.com>
2015-03-03 11:02:56 +0000

    Parse dates lazily and extract some methods for readability

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -3,57 +3,73 @@ require "fixnum"
 
 class DateRangeFormatter
   def initialize(start_date, end_date, start_time = nil, end_time = nil)
-    @start_date = Date.parse(start_date)
-    @end_date = Date.parse(end_date)
+    @raw_start_date = start_date
+    @raw_end_date = end_date
     @start_time = start_time
     @end_time = end_time
   end
 
   def to_s
-    full_start_date = @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
-    full_end_date = @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-
-    if @start_date == @end_date
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} to #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time}"
-      elsif @end_time
-        "#{full_start_date} until #{@end_time}"
+    if start_date == end_date
+      if start_time && end_time
+        "#{full_start_date} at #{start_time} to #{end_time}"
+      elsif start_time
+        "#{full_start_date} at #{start_time}"
+      elsif end_time
+        "#{full_start_date} until #{end_time}"
       else
         full_start_date
       end
-    elsif @start_date.month == @end_date.month
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    elsif start_date.month == end_date.month
+      if start_time && end_time
+        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+      elsif start_time
+        "#{full_start_date} at #{start_time} - #{full_end_date}"
+      elsif end_time
+        "#{full_start_date} - #{full_end_date} at #{end_time}"
       else
-        @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
+        start_date.strftime("#{start_date.day.ordinalize} - #{end_date.day.ordinalize} %B %Y")
       end
-    elsif @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    elsif start_date.year == end_date.year
+      if start_time && end_time
+        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+      elsif start_time
+        "#{full_start_date} at #{start_time} - #{full_end_date}"
+      elsif end_time
+        "#{full_start_date} - #{full_end_date} at #{end_time}"
       else
-        @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+        start_date.strftime("#{start_date.day.ordinalize} %B - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
       end
     else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
+      if start_time && end_time
+        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+      elsif start_time
+        "#{full_start_date} at #{start_time} - #{full_end_date}"
+      elsif end_time
+        "#{full_start_date} - #{full_end_date} at #{end_time}"
       else
         "#{full_start_date} - #{full_end_date}"
       end
     end
+  end
+
+private
+  attr_reader :start_time, :end_time
+
+  def start_date
+    @start_date ||= Date.parse(@raw_start_date)
+  end
+
+  def end_date
+    @end_date ||= Date.parse(@raw_end_date)
+  end
+
+  def full_start_date
+    start_date.strftime("#{start_date.day.ordinalize} %B %Y")
+  end
+
+  def full_end_date
+    end_date.strftime("#{end_date.day.ordinalize} %B %Y")
   end
 end
 
