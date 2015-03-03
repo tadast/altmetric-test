7d070023cf9226c11fc4fef1dc92c51801ac4866
Tadas Tamosauskas <tadas@pdfcv.com>
2015-03-03 11:16:53 +0000

    Break down the first level of conditionals to methods

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -11,45 +11,13 @@ class DateRangeFormatter
 
   def to_s
     if start_date == end_date
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} to #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time}"
-      elsif end_time
-        "#{full_start_date} until #{end_time}"
-      else
-        full_start_date
-      end
+      same_date
     elsif start_date.month == end_date.month
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        start_date.strftime("#{start_date.day.ordinalize} - #{end_date.day.ordinalize} %B %Y")
-      end
+      same_month
     elsif start_date.year == end_date.year
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        start_date.strftime("#{start_date.day.ordinalize} %B - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
-      end
+      same_year
     else
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
-      end
+      no_commonalities
     end
   end
 
@@ -70,6 +38,54 @@ private
 
   def full_end_date
     end_date.strftime("#{end_date.day.ordinalize} %B %Y")
+  end
+
+  def same_date
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} to #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time}"
+    elsif end_time
+      "#{full_start_date} until #{end_time}"
+    else
+      full_start_date
+    end
+  end
+
+  def same_month
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif end_time
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      start_date.strftime("#{start_date.day.ordinalize} - #{end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def same_year
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif end_time
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      start_date.strftime("#{start_date.day.ordinalize} %B - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def no_commonalities
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif end_time
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      "#{full_start_date} - #{full_end_date}"
+    end
   end
 end
 
